571c40c488b8c36ba8ffc87e6d1033d7f6652685
Costinnn <crauciuc.costin@yahoo.com>
2022-12-06 16:42:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/' index element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App
